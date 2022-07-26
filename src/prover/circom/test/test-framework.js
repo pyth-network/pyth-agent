500101f267d68e4ad552e84df0d3eb64505d95dd
Tom Pointon <tom@teepeestudios.net>
2022-07-26 16:05:06 +0000

    WIP on test framework

--- src/prover/circom/test/test-framework.js
+++ src/prover/circom/test/test-framework.js
@@ -4,8 +4,47 @@ const path = require("path");
 const c_tester = require("circom_tester").c;
 const { randomBytes } = require("crypto");
 const { buildEddsa } = require("circomlibjs");
+const { assert } = require("console");
+
+/*
+Test cases:
+
+- Valid signature
+    - Good case
+
+- Invalid signature
+    - Private and public key mismatch
+    - Data included in signature is the wrong size
 
 
+*/
+
+// Utility Functions
+        const toBinString = (bytes) =>
+            bytes.reduce(
+                (str, byte) =>
+                str + byte.toString(2).padStart(8, "0"),
+                ""
+            );
+
+        const fromBinString = (binString) =>
+            new Uint8Array(binString.match(/.{1,8}/g).map((byte) => parseInt(byte, 2)));
+
+        const conv = num => {
+            let b = new ArrayBuffer(8);
+            new DataView(b).setBigUint64(0, num);
+            return Array.from(new Uint8Array(b));
+        };
+
+        const toCircomBin = (bytes) =>
+            bytes.reduce(
+                (str, byte) =>
+                str + byte.toString(2).padStart(8, "0").split("").reverse().join(""),
+                ""
+        ).split('').map(v => Number(v));
+
+        const pad64BitArray = arr =>
+            arr.concat(Array(64 - arr.length).fill(0));
 
 describe("Test Input Verifier", function () {
 
@@ -62,34 +101,7 @@ component main = InputVerifier();
         */
 
         // Generate signature
-        privateKey = randomBytes(32);
-
-        // Generate components
-        const toBinString = (bytes) =>
-            bytes.reduce(
-                (str, byte) =>
-                str + byte.toString(2).padStart(8, "0"),
-                ""
-            );
-
-        const fromBinString = (binString) =>
-            new Uint8Array(binString.match(/.{1,8}/g).map((byte) => parseInt(byte, 2)));
-
-        const conv = num => {
-            let b = new ArrayBuffer(8);
-            new DataView(b).setBigUint64(0, num);
-            return Array.from(new Uint8Array(b));
-        };
-
-        const toCircomBin = (bytes) =>
-            bytes.reduce(
-                (str, byte) =>
-                str + byte.toString(2).padStart(8, "0").split("").reverse().join(""),
-                ""
-        ).split('').map(v => Number(v));
-
-        const pad64BitArray = arr =>
-            arr.concat(Array(64 - arr.length).fill(0));
+        privateKey = randomBytes(32);  
 
         // Generate some random input values
         let price = [6]
@@ -103,11 +115,15 @@ component main = InputVerifier();
             toBinString(conv(BigInt(price)).reverse()) +
             toBinString(conv(BigInt(conf)).reverse()) +
             toBinString(conv(BigInt(timestamp)).reverse());
-        const signature = eddsa.signPedersen(privateKey, fromBinString(payload));
+
+        // Generate A (public key)
         const pubKey = eddsa.prv2pub(privateKey);
-        const packedPubKey = eddsa.babyJub.packPoint(pubKey);
-        const packedSignature = eddsa.packSignature(signature);
+        const packedPubKey = eddsa.babyJub.packPoint(pubKey);  
         const A = packedPubKey;
+                
+        // Generate signature component (R S)
+        const signature = eddsa.signPedersen(privateKey, fromBinString(payload));
+        const packedSignature = eddsa.packSignature(signature);
         const R = packedSignature.slice(0, 32);
         const S = packedSignature.slice(32, 64);
 
@@ -131,6 +147,62 @@ component main = InputVerifier();
         await circuit.assertOut(witness, {});
     });
     
+it ('there should fail if public key is invalid', async() => {        
+    /*
+    Inputs to InputVerifier:
+    A/R/S: one set of ed25519 signature components
+    price: 64-bit signed binary price input
+    confidence: 64-bit signed binary confidence input
+    timestamp: 64-bit signed binary timestamp input
+    online: 64-bit signed binary online input
+    */
+
+    // Generate signature
+    privateKey = randomBytes(32);
+
+    // Generate some random input values
+    let price = [6]
+    let conf = [10];
+    let timestamp = [75463746];
+            
+    // Sign components
+    // Message payload: 
+    // packed array of price (64-bit), conf (64-bit), timestamp (64-bit), online (64-bit)
+    const payload = 
+        toBinString(conv(BigInt(price)).reverse()) +
+        toBinString(conv(BigInt(conf)).reverse()) +
+        toBinString(conv(BigInt(timestamp)).reverse());
+
+        // Generate A (public key)
+        const pubKey = eddsa.prv2pub(privateKey);
+        const pubKeyInvalid = randomBytes(32);
+        assert(pubKey != pubKeyInvalid);
+
+        const packedPubKey = eddsa.babyJub.packPoint(pubKey);  
+        const A = packedPubKey;
+            
+    // Generate signature component (R S)
+    const signature = eddsa.signPedersen(privateKey, fromBinString(payload));
+    const packedSignature = eddsa.packSignature(signature);
+    const R = packedSignature.slice(0, 32);
+    const S = packedSignature.slice(32, 64);
+
+    // Input object
+    let input = {
+        'A': toCircomBin(A),
+        'R': toCircomBin(R),
+        'S': toCircomBin(S),
+        'price': pad64BitArray(toCircomBin(price)),
+        'confidence': pad64BitArray(toCircomBin(conf)),
+        'timestamp': pad64BitArray(toCircomBin(timestamp)),
+    }
+
+    console.log('----- Input ------')
+    console.log(input)
+
+    // Constraints should 
+    assert.throws(await circuit.calculateWitness(input, true), Error, "msg")
+});
     
     // it ('should verify the signatures correctly passed in', async() => {
          
