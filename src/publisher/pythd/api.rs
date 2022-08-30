9f3a3d313bc8269e09b171a9c90ea2b1d393e257
Tom Pointon <tom@teepeestudios.net>
2022-08-30 18:32:55 +0000

    Add publisher and pythd modules

---
+++ src/publisher/pythd/api.rs
@@ -0,0 +1,5 @@
+// This module is responsible for exposing the JRPC-esq websocket API
+// documented at https://docs.pyth.network/publish-data/pyth-client-websocket-api
+//
+// It does not implement the business logic, only exposes a websocket server which
+// accepts messages and can return responses in the expected format.
