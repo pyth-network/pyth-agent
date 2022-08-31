33a8df72f2d463188c662a16386f7f6c3aa72292
Tom Pointon <tom@teepeestudios.net>
2022-08-31 10:57:41 +0000

    Add pythd adapter module and message formats

---
+++ src/publisher/pythd/adapter.rs
@@ -0,0 +1,39 @@
+use super::api::{
+    Conf, NotifyPrice, NotifyPriceSched, Price, PriceAccount, PriceAccountMetadata, ProductAccount,
+    ProductAccountMetadata, PubKey, PublisherAccount, SubscriptionID,
+};
+use tokio::sync::mpsc::{Receiver, Sender};
+use tokio::sync::{mpsc, oneshot};
+
+// Adapter is the adapter between the pythd websocket API, and the stores.
+// It is responsible for implementing the business logic for responding to
+// the pythd websocket API calls.
+#[derive(Debug)]
+pub enum Message {
+    GetProductList {
+        result_tx: oneshot::Sender<Result<Vec<ProductAccountMetadata>>>,
+    },
+    GetProduct {
+        account: PubKey,
+        result_tx: oneshot::Sender<Result<ProductAccount>>,
+    },
+    GetAllProducts {
+        result_tx: oneshot::Sender<Result<Vec<ProductAccount>>>,
+    },
+    SubscribePrice {
+        account: PubKey,
+        notify_price_tx: mpsc::Sender<NotifyPrice>,
+        result_tx: oneshot::Sender<Result<SubscriptionID>>,
+    },
+    SubscribePriceSched {
+        account: PubKey,
+        notify_price_sched_tx: mpsc::Sender<NotifyPriceSched>,
+        result_tx: oneshot::Sender<Result<SubscriptionID>>,
+    },
+    UpdatePrice {
+        account: PubKey,
+        price: Price,
+        conf: Conf,
+        status: String,
+    },
+}
