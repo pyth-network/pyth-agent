a87807b1b532b7abcd487ae4f4b849129c52d8ea
Tom Pointon <tom@teepeestudios.net>
2022-11-01 14:29:01 +0000

    Add scaffolding to allow Adapter to process messages (#4)
    * Add scaffolding to allow Adapter to process messages
    
    * Add shutdown channel
    
    * Improve log message
    
    * Don't return error from run

--- src/publisher/pythd/adapter.rs
+++ src/publisher/pythd/adapter.rs
@@ -1,13 +1,33 @@
+use std::{collections::HashMap, time::Duration};
+
 use super::api::{
     Conf, NotifyPrice, NotifyPriceSched, Price, ProductAccount, ProductAccountMetadata, Pubkey,
     SubscriptionID,
 };
 use anyhow::Result;
-use tokio::sync::{mpsc, oneshot};
+use slog::Logger;
+use tokio::{
+    sync::{broadcast, mpsc, oneshot},
+    time::{self, Interval},
+};
 
-// Adapter is the adapter between the pythd websocket API, and the stores.
-// It is responsible for implementing the business logic for responding to
-// the pythd websocket API calls.
+/// Adapter is the adapter between the pythd websocket API, and the stores.
+/// It is responsible for implementing the business logic for responding to
+/// the pythd websocket API calls.
+pub struct Adapter {
+    /// Channel on which messages are received
+    message_rx: mpsc::Receiver<Message>,
+
+    /// The fixed interval at which Notify Price Sched notifications are sent
+    notify_price_sched_interval: Interval,
+
+    /// Channel on which the shutdown is broadcast
+    shutdown_rx: broadcast::Receiver<()>,
+
+    /// The logger
+    logger: Logger,
+}
+
 #[derive(Debug)]
 pub enum Message {
     GetProductList {
@@ -36,4 +56,63 @@ pub enum Message {
         conf: Conf,
         status: String,
     },
+}
+
+impl Adapter {
+    pub fn new(
+        message_rx: mpsc::Receiver<Message>,
+        notify_price_sched_interval: Duration,
+        shutdown_rx: broadcast::Receiver<()>,
+        logger: Logger,
+    ) -> Self {
+        Adapter {
+            message_rx,
+            notify_price_sched_interval: time::interval(notify_price_sched_interval),
+            shutdown_rx,
+            logger,
+        }
+    }
+
+    pub async fn run(&mut self) {
+        loop {
+            tokio::select! {
+                Some(message) = self.message_rx.recv() => {
+                    if let Err(err) = self.handle_message(message).await {
+                        error!(self.logger, "{:#}", err; "error" => format!("{:?}", err))
+                    }
+                }
+                _ = self.shutdown_rx.recv() => {
+                    info!(self.logger, "shutdown signal received");
+                    return;
+                }
+                _ = self.notify_price_sched_interval.tick() => {
+                    todo!()
+                }
+            };
+        }
+    }
+
+    async fn handle_message(&mut self, message: Message) -> Result<()> {
+        match message {
+            Message::GetProductList { result_tx } => todo!(),
+            Message::GetProduct { account, result_tx } => todo!(),
+            Message::GetAllProducts { result_tx } => todo!(),
+            Message::SubscribePrice {
+                account,
+                notify_price_tx,
+                result_tx,
+            } => todo!(),
+            Message::SubscribePriceSched {
+                account,
+                notify_price_sched_tx,
+                result_tx,
+            } => todo!(),
+            Message::UpdatePrice {
+                account,
+                price,
+                conf,
+                status,
+            } => todo!(),
+        }
+    }
 }
