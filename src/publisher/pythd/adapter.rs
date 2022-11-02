1ee7971aa22e8fcbdcf18c223f8a18f2810f5f0a
Tom Pointon <tom@teepeestudios.net>
2022-11-02 10:01:33 +0000

    Add notify_price_sched handler to Adapter (#5)
    * Use scoped types to distinguish between different Pubkeys
    
    * Add notify_price_sched handler
    
    * Add test for notify_price_sched_handler
    
    * Rename method
    
    * Remove assert

--- src/publisher/pythd/adapter.rs
+++ src/publisher/pythd/adapter.rs
@@ -1,10 +1,11 @@
 use std::{collections::HashMap, time::Duration};
 
 use super::api::{
-    Conf, NotifyPrice, NotifyPriceSched, Price, ProductAccount, ProductAccountMetadata, Pubkey,
+    self, Conf, NotifyPrice, NotifyPriceSched, Price, ProductAccount, ProductAccountMetadata,
     SubscriptionID,
 };
-use anyhow::Result;
+use anyhow::{anyhow, Result};
+use pyth_sdk::{Identifier, PriceIdentifier};
 use slog::Logger;
 use tokio::{
     sync::{broadcast, mpsc, oneshot},
@@ -18,6 +19,12 @@ pub struct Adapter {
     /// Channel on which messages are received
     message_rx: mpsc::Receiver<Message>,
 
+    /// Subscription ID counter
+    subscription_id_count: SubscriptionID,
+
+    /// Notify Price Sched subscriptions
+    notify_price_sched_subscriptions: HashMap<PriceIdentifier, Vec<NotifyPriceSchedSubscription>>,
+
     /// The fixed interval at which Notify Price Sched notifications are sent
     notify_price_sched_interval: Interval,
 
@@ -28,30 +35,38 @@ pub struct Adapter {
     logger: Logger,
 }
 
+/// Represents a single Notify Price Sched subscription
+struct NotifyPriceSchedSubscription {
+    /// ID of this subscription
+    subscription_id: SubscriptionID,
+    /// Channel notifications are sent on
+    notify_price_sched_tx: mpsc::Sender<NotifyPriceSched>,
+}
+
 #[derive(Debug)]
 pub enum Message {
     GetProductList {
         result_tx: oneshot::Sender<Result<Vec<ProductAccountMetadata>>>,
     },
     GetProduct {
-        account: Pubkey,
+        account: api::Pubkey,
         result_tx: oneshot::Sender<Result<ProductAccount>>,
     },
     GetAllProducts {
         result_tx: oneshot::Sender<Result<Vec<ProductAccount>>>,
     },
     SubscribePrice {
-        account: Pubkey,
+        account: api::Pubkey,
         notify_price_tx: mpsc::Sender<NotifyPrice>,
         result_tx: oneshot::Sender<Result<SubscriptionID>>,
     },
     SubscribePriceSched {
-        account: Pubkey,
+        account: api::Pubkey,
         notify_price_sched_tx: mpsc::Sender<NotifyPriceSched>,
         result_tx: oneshot::Sender<Result<SubscriptionID>>,
     },
     UpdatePrice {
-        account: Pubkey,
+        account: api::Pubkey,
         price: Price,
         conf: Conf,
         status: String,
@@ -67,6 +82,8 @@ impl Adapter {
     ) -> Self {
         Adapter {
             message_rx,
+            subscription_id_count: 0,
+            notify_price_sched_subscriptions: HashMap::new(),
             notify_price_sched_interval: time::interval(notify_price_sched_interval),
             shutdown_rx,
             logger,
@@ -86,9 +103,11 @@ impl Adapter {
                     return;
                 }
                 _ = self.notify_price_sched_interval.tick() => {
-                    todo!()
+                    if let Err(err) = self.send_notify_price_sched().await {
+                        error!(self.logger, "{:#}", err; "error" => format!("{:?}", err))
+                    }
                 }
-            };
+            }
         }
     }
 
@@ -106,13 +125,133 @@ impl Adapter {
                 account,
                 notify_price_sched_tx,
                 result_tx,
-            } => todo!(),
+            } => {
+                let subscription_id = self
+                    .handle_subscribe_price_sched(&account.parse()?, notify_price_sched_tx)
+                    .await;
+                let res = self.send(result_tx, Ok(subscription_id));
+                res
+            }
             Message::UpdatePrice {
                 account,
                 price,
                 conf,
                 status,
             } => todo!(),
+        }
+    }
+
+    fn send<T>(&self, tx: oneshot::Sender<T>, item: T) -> Result<()> {
+        tx.send(item).map_err(|_| anyhow!("sending channel full"))
+    }
+
+    async fn handle_subscribe_price_sched(
+        &mut self,
+        account_pubkey: &solana_sdk::pubkey::Pubkey,
+        notify_price_sched_tx: mpsc::Sender<NotifyPriceSched>,
+    ) -> SubscriptionID {
+        let subscription_id = self.next_subscription_id();
+        self.notify_price_sched_subscriptions
+            .entry(Identifier::new(account_pubkey.to_bytes()))
+            .or_default()
+            .push(NotifyPriceSchedSubscription {
+                subscription_id,
+                notify_price_sched_tx,
+            });
+        subscription_id
+    }
+
+    fn next_subscription_id(&mut self) -> SubscriptionID {
+        self.subscription_id_count += 1;
+        self.subscription_id_count
+    }
+
+    async fn send_notify_price_sched(&self) -> Result<()> {
+        for subscription in self.notify_price_sched_subscriptions.values().flatten() {
+            subscription
+                .notify_price_sched_tx
+                .send(NotifyPriceSched {
+                    subscription: subscription.subscription_id,
+                })
+                .await?;
+        }
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use iobuffer::IoBuffer;
+    use slog_extlog::slog_test;
+    use std::time::Duration;
+    use tokio::{
+        sync::{broadcast, mpsc, oneshot},
+        task::JoinHandle,
+    };
+
+    use crate::publisher::pythd::api::NotifyPriceSched;
+
+    use super::{Adapter, Message};
+
+    struct TestAdapter {
+        message_tx: mpsc::Sender<Message>,
+        shutdown_tx: broadcast::Sender<()>,
+        jh: JoinHandle<()>,
+    }
+
+    impl Drop for TestAdapter {
+        fn drop(&mut self) {
+            let _ = self.shutdown_tx.send(());
+            self.jh.abort();
+        }
+    }
+
+    async fn setup() -> TestAdapter {
+        // Create and spawn an adapter
+        let (adapter_tx, adapter_rx) = mpsc::channel(100);
+        let notify_price_sched_interval = Duration::from_nanos(10);
+        let logger = slog_test::new_test_logger(IoBuffer::new());
+        let (shutdown_tx, shutdown_rx) = broadcast::channel(10);
+        let mut adapter =
+            Adapter::new(adapter_rx, notify_price_sched_interval, shutdown_rx, logger);
+        let jh = tokio::spawn(async move { adapter.run().await });
+
+        TestAdapter {
+            message_tx: adapter_tx,
+            shutdown_tx,
+            jh,
+        }
+    }
+
+    #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
+    async fn test_subscribe_price_sched() {
+        let test_adapter = setup().await;
+
+        // Send a Subscribe Price Sched message
+        let account = "2wrWGm63xWubz7ue4iYR3qvBbaUJhZVi4eSpNuU8k8iF".to_string();
+        let (notify_price_sched_tx, mut notify_price_sched_rx) = mpsc::channel(1000);
+        let (result_tx, result_rx) = oneshot::channel();
+        test_adapter
+            .message_tx
+            .send(Message::SubscribePriceSched {
+                account,
+                notify_price_sched_tx,
+                result_tx,
+            })
+            .await
+            .unwrap();
+
+        let subscription_id = result_rx.await.unwrap().unwrap();
+
+        // Expect that we recieve several Notify Price Sched notifications
+        for _ in 0..10 {
+            assert_eq!(
+                notify_price_sched_rx.recv().await.unwrap(),
+                NotifyPriceSched {
+                    subscription: subscription_id
+                }
+            )
         }
     }
 }
