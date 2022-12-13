49dd38d09bb15881bf7ca11c71aa8e423958fee0
Tom Pointon <tom@teepeestudios.net>
2022-12-13 16:43:31 +0000

    Make filenames configurable

--- src/publisher/solana/exporter.rs
+++ src/publisher/solana/exporter.rs
@@ -13,10 +13,24 @@ mod key_store {
         },
         std::{
             fs,
-            path::Path,
+            path::{
+                Path,
+                PathBuf,
+            },
             str::FromStr,
         },
     };
+
+    pub struct Config {
+        /// Root directory of the KeyStore
+        pub root_path:            PathBuf,
+        /// Path to the keypair used to publish price updates, relative to the root
+        pub publish_keypair_path: PathBuf,
+        /// Path to the public key of the Oracle program, relative to the root
+        pub program_key_path:     PathBuf,
+        /// Path to the public key of the root mapping account, relative to the root
+        pub mapping_key_path:     PathBuf,
+    }
 
     pub struct KeyStore {
         /// The keypair used to publish price updates
@@ -28,17 +42,21 @@ mod key_store {
     }
 
     impl KeyStore {
-        pub fn new(root: impl AsRef<Path>) -> Result<Self> {
+        pub fn new(config: Config) -> Result<Self> {
             Ok(KeyStore {
                 publish_keypair: keypair::read_keypair_file(
-                    root.as_ref().join("publish_key_pair.json"),
+                    config.root_path.join(config.publish_keypair_path),
                 )
                 .map_err(|e| anyhow!(e.to_string()))
                 .context("reading publish keypair")?,
-                program_key:     Self::pubkey_from_path(root.as_ref().join("program_key.json"))
-                    .context("reading program key")?,
-                mapping_key:     Self::pubkey_from_path(root.as_ref().join("mapping_key.json"))
-                    .context("reading mapping key")?,
+                program_key:     Self::pubkey_from_path(
+                    config.root_path.join(config.program_key_path),
+                )
+                .context("reading program key")?,
+                mapping_key:     Self::pubkey_from_path(
+                    config.root_path.join(config.mapping_key_path),
+                )
+                .context("reading mapping key")?,
             })
         }
 
