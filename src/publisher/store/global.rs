5e5b1cfb9323b277a252452677363916a0130e3e
Tom Pointon <tom@teepeestudios.net>
2022-12-14 16:43:35 +0000

    Update global store message format

--- src/publisher/store/global.rs
+++ src/publisher/store/global.rs
@@ -1,69 +1,102 @@
 // The Global Store stores a copy of all the product and price information held in the Pyth
 // on-chain aggregation contracts, across both the primary and secondary networks.
 // This enables this data to be easily queried by other components.
-
 use {
-    super::{
-        super::solana::oracle::{
-            self,
-            PriceAccount,
-            ProductAccount,
-        },
-        PriceIdentifier,
+    super::super::solana::oracle::{
+        self,
+        PriceAccount,
+        ProductAccount,
     },
-    anyhow::Result,
-    pyth_sdk::PriceFeed,
+    crate::publisher::pythd::adapter,
+    anyhow::{
+        anyhow,
+        Result,
+    },
+    pyth_sdk::Identifier,
+    slog::Logger,
+    solana_sdk::pubkey::Pubkey,
     std::collections::{
         BTreeMap,
         HashMap,
     },
-    tokio::sync::oneshot,
+    tokio::sync::{
+        mpsc,
+        oneshot,
+    },
 };
 
-/// AllAccountsMetadata contains the metadata for all the price and product accounts,
-/// merged from both networks.
+/// AllAccountsData contains the full data for the price and product accounts, sourced
+/// from the primary network.
+#[derive(Debug, Clone)]
+pub struct AllAccountsData {
+    pub product_accounts: HashMap<Pubkey, oracle::ProductAccount>,
+    pub price_accounts:   HashMap<Pubkey, oracle::PriceAccount>,
+}
+
+/// AllAccountsMetadata contains the metadata for all the price and product accounts.
 ///
-/// Crucially, this relies on the metadata for all accounts being consistent across both networks.
-#[derive(Debug)]
+/// Important: this relies on the metadata for all accounts being consistent across both networks.
+#[derive(Debug, Clone)]
 pub struct AllAccountsMetadata {
-    pub product_accounts_metadata: HashMap<solana_sdk::pubkey::Pubkey, ProductAccountMetadata>,
-    pub price_accounts_metadata:   HashMap<solana_sdk::pubkey::Pubkey, PriceAccountMetadata>,
+    pub product_accounts_metadata: HashMap<Pubkey, ProductAccountMetadata>,
+    pub price_accounts_metadata:   HashMap<Pubkey, PriceAccountMetadata>,
 }
 
 /// ProductAccountMetadata contains the metadata for a product account.
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct ProductAccountMetadata {
     /// Attribute dictionary
     pub attr_dict:      BTreeMap<String, String>,
     /// Price accounts associated with this product
-    pub price_accounts: Vec<solana_sdk::pubkey::Pubkey>,
+    pub price_accounts: Vec<Pubkey>,
+}
+
+impl From<oracle::ProductAccount> for ProductAccountMetadata {
+    fn from(product_account: oracle::ProductAccount) -> Self {
+        ProductAccountMetadata {
+            attr_dict:      product_account
+                .account_data
+                .iter()
+                .map(|(key, val)| (key.to_owned(), val.to_owned()))
+                .collect(),
+            price_accounts: product_account.price_accounts,
+        }
+    }
 }
 
 /// PriceAccountMetadata contains the metadata for a price account.
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct PriceAccountMetadata {
     /// Exponent
     pub expo: i32,
 }
 
+impl From<oracle::PriceAccount> for PriceAccountMetadata {
+    fn from(price_account: oracle::PriceAccount) -> Self {
+        PriceAccountMetadata {
+            expo: price_account.expo,
+        }
+    }
+}
+
 #[derive(Debug)]
-pub enum Message {
+pub enum Update {
     ProductAccountUpdate {
-        account_key: solana_sdk::pubkey::Pubkey,
+        account_key: Pubkey,
         account:     ProductAccount,
     },
     PriceAccountUpdate {
-        account_key: solana_sdk::pubkey::Pubkey,
+        account_key: Pubkey,
         account:     PriceAccount,
     },
+}
+
+#[derive(Debug)]
+pub enum Lookup {
     LookupAllAccountsMetadata {
         result_tx: oneshot::Sender<Result<AllAccountsMetadata>>,
     },
-    LookupPriceFeed {
-        identifier: PriceIdentifier,
-        result_tx:  oneshot::Sender<Result<PriceFeed>>,
-    },
-    LookupSolanaOracleData {
-        result_tx: oneshot::Sender<Result<oracle::Data>>,
+    LookupAllAccountsData {
+        result_tx: oneshot::Sender<Result<AllAccountsData>>,
     },
 }
