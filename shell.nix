3f9246522a1aa7c324fca78e99ca1235e5753765
Stan Drozd <stan@pyth.network>
2023-03-23 18:38:53 +0100

    nix: nixpkgs release-22.11, solana validator 1.15.2, /usr/bin/env

--- shell.nix
+++ shell.nix
@@ -1,17 +1,32 @@
-{ pkgs ? import <nixpkgs> {}
+{ pkgs ? import <nixpkgs> { }
 , ...
 }:
+let
+  solana-validator-bumped = pkgs.callPackage ./nix/solana-validator.nix {
+    inherit (pkgs.apple_sdk.frameworks) IOKit Security AppKit;
+    rustPlatform = pkgs.rustPackages_1_66.rustPlatform;
+  };
 
-with pkgs; mkShell {
+in
+with pkgs;
+mkShell rec {
   buildInputs = [
     clang
     llvmPackages.libclang
+    git # the shell may be using a different glibc which upsets common CLI tools
     openssl
     pkgconfig
     rustup
+    python3
+    poetry
+    solana-validator-bumped
+    stdenv.cc
+    stdenv.cc.cc.lib # libstdc++.so.6
   ];
 
-  shellHook = ''
-    export LIBCLANG_PATH="${llvmPackages.libclang.lib}/lib";
-  '';
+  # Explicitly tell the environment about all *.so's (libstdcxx, libclang end up in the path this way)
+  LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
+
+  # Tell the Rust build where libclang lives
+  LIBCLANG_PATH = "${llvmPackages.libclang.lib}/lib";
 }
