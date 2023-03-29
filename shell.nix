b1e1c45a45600b7d6a1f251de790f5bda75ddefd
Stan Drozd <stan@pyth.network>
2023-03-29 16:51:58 +0200

    Remove more stray nix changes

--- shell.nix
+++ shell.nix
@@ -1,32 +1,17 @@
-{ pkgs ? import <nixpkgs> { }
+{ pkgs ? import <nixpkgs> {}
 , ...
 }:
-let
-  solana-validator-bumped = pkgs.callPackage ./nix/solana-validator.nix {
-    inherit (pkgs.apple_sdk.frameworks) IOKit Security AppKit;
-    rustPlatform = pkgs.rustPackages_1_66.rustPlatform;
-  };
 
-in
-with pkgs;
-mkShell rec {
+with pkgs; mkShell {
   buildInputs = [
     clang
     llvmPackages.libclang
-    git # the shell may be using a different glibc which upsets common CLI tools
     openssl
     pkgconfig
     rustup
-    python3
-    poetry
-    solana-validator-bumped
-    stdenv.cc
-    stdenv.cc.cc.lib # libstdc++.so.6
   ];
 
-  # Explicitly tell the environment about all *.so's (libstdcxx, libclang end up in the path this way)
-  LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
-
-  # Tell the Rust build where libclang lives
-  LIBCLANG_PATH = "${llvmPackages.libclang.lib}/lib";
+  shellHook = ''
+    export LIBCLANG_PATH="${llvmPackages.libclang.lib}/lib";
+  '';
 }
