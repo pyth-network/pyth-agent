75497df9f0fc43d3291995a827061d713da65fc4
Stanisław Drozd <drozdziak1@gmail.com>
2023-11-23 14:07:33 +0100

    src/agent/market_hours.rs: Add market hours format implementation (#93)
    * src/agent/market_hours.rs: Add market hours format implementation
    
    * market_hours.rs: Support and test 24:00 as an open hours end time.
    
    * Please the formatter
    
    * Add missing O in midnight test

--- src/agent.rs
+++ src/agent.rs
@@ -63,6 +63,7 @@ Note that there is an Oracle and Exporter for each network, but only one Local S
 ################################################################################################################################## */
 
 pub mod dashboard;
+pub mod market_hours;
 pub mod metrics;
 pub mod pythd;
 pub mod remote_keypair_loader;
