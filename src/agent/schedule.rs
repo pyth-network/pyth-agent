3c9ee2ef7eef1084b67bf13963514e722808f331
keyvan <keyvankhademi@gmail.com>
2024-04-10 09:28:27 -0700

    refactor: imports

--- src/agent/schedule.rs
+++ src/agent/schedule.rs
@@ -1,11 +1,8 @@
+use holiday_hours::HolidaySchedule;
+use market_hours::WeeklySchedule;
+
 pub mod holiday_hours;
 pub mod market_hours;
-
-
-use crate::agent::schedule::{
-    holiday_hours::HolidaySchedule,
-    market_hours::WeeklySchedule,
-};
 
 #[derive(Debug, Clone, Default)]
 pub struct Schedule {
